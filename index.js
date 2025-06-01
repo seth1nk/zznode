e3fea699b0054460c3d5eaf56968c8c3dd4de95d
seth1nk <kostik.vs10@mail.ru>
2025-06-01 13:49:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://gorgeous-chebakia-3d6a9c.netlify.app',
     credentials: true,
 }));
 app.use('/css', express.static(path.join(__dirname, 'css')));
